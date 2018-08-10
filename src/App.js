16a99905e67b4cd1ac549b05b5c00a08d6efda52
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2018-08-10 19:31:43 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -52,7 +52,14 @@ class App extends Component {
   render() {
     return (
       <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+        <div className="col-xs-5 title-container">
         <Title />
+        </div>
+        <div className="col-xs-7 form-container">
         <Form getWeather={this.getWeather}/>
         <Weather 
         temperature = {this.state.temperature}
@@ -62,6 +69,11 @@ class App extends Component {
         description = {this.state.description}
         error = {this.state.error}
         />
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
